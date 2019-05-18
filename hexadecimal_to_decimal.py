0b637d3406652aacdcae047363113b6efbb3e9b0
Nazmul Hossain <nazmulhossain914@gmail.com>
2019-05-18 02:16:56 -0700

    Add files via upload

---
+++ hexadecimal_to_decimal.py
@@ -0,0 +1,26 @@
+
+
+##############################################
+#											 #
+#	 Convert From Hexadecimal To Decimal	 #
+#											 #
+##############################################
+
+
+def con(n,b):
+	x = n.upper()	 # If user input small char it will convert into upper char
+	d = len(n)-1
+	l = 0
+	for i in x:
+		if(i > "F"):	 # User may input more than 'f/F' which is incorrect.
+			print("Invalid Input According To Base")
+			exit()
+		else:
+			h = int(i, 16)
+			l += h*(b**d)		# Formula to convert the base
+			d = d-1
+	print(l)					# Printing the result
+
+n = input("Enter The Hexadecimal Value: ")
+b = 16
+con(n,b)
